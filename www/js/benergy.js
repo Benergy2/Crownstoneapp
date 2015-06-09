070f82018a0afdf319a7b24d3409c23e886363d5
Ruben de Man <rubenodeman@gmail.com>
2015-06-09 14:45:35 +0200

    Basis file JS + eerste clickhandlers

---
+++ www/js/benergy.js
@@ -0,0 +1,22 @@
+/**
+ * Created by rubendeman on 09-06-15.
+ */
+$('document').ready(init);
+function init(){
+
+    // Login button
+    $('.loginButton').on('click', function(){
+        
+    });
+
+    // Register button
+    $('.registerButton').on('click', function(){
+
+    });
+
+    // lostPassword button
+    $('.lostPasswordButton').on('click', function(){
+
+    });
+
+}
