3aff7ef28325d655f1f22c471d4f200186fb5b26
teutates1989 <cristiaan1989@gmail.com>
2015-06-10 11:07:59 +0200

    android build
    build

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -1,14 +1,6 @@
 D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\ant-gen\nl\dobots\CrownStone\R.java \
  : D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\res\drawable\icon.png \
-D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\res\drawable-hdpi\icon.png \
-D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\res\drawable-ldpi\icon.png \
-D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\res\drawable-mdpi\icon.png \
-D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\res\drawable-xhdpi\icon.png \
 D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\res\values\strings.xml \
 D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\res\xml\config.xml \
 D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\ant-build\res\drawable\icon.png \
-D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\ant-build\res\drawable-hdpi\icon.png \
-D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\ant-build\res\drawable-ldpi\icon.png \
-D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\ant-build\res\drawable-mdpi\icon.png \
-D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 D:\Users\Cristiaan\Documents\GitHub\Crownstoneapp\platforms\android\ant-build\AndroidManifest.xml \
---
+++ platforms/android/assets/www/js/benergy.js
@@ -0,0 +1,61 @@
+/**
+ * Created by rubendeman on 09-06-15.
+ */
+$('document').ready(init);
+function init(){
+
+    // Login button
+    $('.loginButton').on('click', function(){
+        var username =  $(".text1").val();
+        var password =  $(".text2").val();
+        $.ajax({
+            type: 'GET',
+            url: 'http://rubendeman.nl/api/index.php',
+            contentType: 'application/json; charset=utf-8',
+            data: {action: "login", username: username, password: password },
+            dataType: 'json',
+            success: function(data) {
+                alert('Uw bent nu ingelogd');
+                localStorage.setItem("user_id", data['id']);
+            },
+            error: function(data) {
+                alert('Er is iets misgegaan');
+                console.log(data);
+            }
+        });
+
+    });
+
+    // Register button
+    $('.registerButton').on('click', function(){
+        var email =  $(".text1").val();
+        var username =  $(".text2").val();
+        var password =  $(".text3").val();
+        var dorm = $('.select1').val();
+
+        var key = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for( var i=0; i < 50; i++ )
+            key += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        $.ajax({
+            type: 'GET',
+            url: 'http://rubendeman.nl/api/index.php',
+            contentType: 'application/json; charset=utf-8',
+            data: {action: "register", email: email, username: username, password: password, dorm: dorm,key: key},
+            dataType: 'json',
+            success: function(data) {
+                alert('account is aangemaakt');
+            },
+            error: function(data) {
+                alert('Er is iets misgegaan'+this.url);
+            }
+        });
+    });
+
+    // lostPassword button
+    $('.lostPasswordButton').on('click', function(){
+
+    });
+
+}
